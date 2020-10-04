b003afaed473d3feaac5c0b5129bb5899284e490
Raphael Barreto <tnt.raphael@gmail.com>
2020-10-04 13:22:29 -0300

    Adicionado ESLint + Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-
-function App() {
-  return (
-   <h1>Hello, Testa</h1>
-  );
-}
+const App: React.FC = () => {
+  return <h1>Hello, Tesla</h1>;
+};
 
 export default App;
